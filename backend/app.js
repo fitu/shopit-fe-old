48fb02328e8a60d7d56f5b423a7603bae05bd669
Victorio Matteucci <victorio.matteucci@simplex.software>
2021-03-26 19:49:12 -0300

    [feat]: add orders

--- backend/app.js
+++ backend/app.js
@@ -12,10 +12,12 @@ app.use(cookieParser());
 // Import all routes
 const productsRoute = require('./routes/productsRoute');
 const authRoute = require('./routes/authRoute');
+const orderRoute = require('./routes/orderRoute');
 
 // Set routes
 app.use('/api/v1', productsRoute);
 app.use('/api/v1', authRoute);
+app.use('/api/v1', orderRoute);
 
 // Middleware errors
 const errorMiddleware = require('./middlewares/errors');
