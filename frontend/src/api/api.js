737b6aa4c4f355a8f1b332c55344db9c92921d1b
Victorio Matteucci <victorio.matteucci@simplex.software>
2021-04-08 20:04:58 -0300

    [imp]: uris and urls

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,135 +1,126 @@
 import axios from 'axios';
 
+const BASE_URI_VERSION = '/api/v1';
+const baseHeaders = { headers: { 'Content-type': 'application/json' } };
+
 export function login(email, password) {
-    const config = { headers: { 'Content-type': 'application/json' } };
-    return axios.post('/api/v1/login', { email, password }, config);
+    return axios.post(`${BASE_URI_VERSION}/login`, { email, password }, baseHeaders);
 }
 
 export function register(userData) {
-    const config = { headers: { 'Content-type': 'multipart/from-data' } };
-    return axios.post('/api/v1/register', userData, config);
+    const customHeaders = { headers: { 'Content-type': 'multipart/from-data' } };
+    return axios.post(`${BASE_URI_VERSION}/register`, userData, customHeaders);
 }
 
 export function loadUser() {
-    return axios.get('/api/v1/me');
+    return axios.get(`${BASE_URI_VERSION}/me`);
 }
 
 export function logoutUser() {
-    return axios.get('/api/v1/logout');
+    return axios.get(`${BASE_URI_VERSION}/logout`);
 }
 
 export function updateProfile(userData) {
-    const config = { headers: { 'Content-type': 'multipart/from-data' } };
-    return axios.put('/api/v1/me/update', userData, config);
+    return axios.put(`${BASE_URI_VERSION}/me/update`, userData, baseHeaders);
 }
 
 export function updatePassword(passwords) {
-    const config = { headers: { 'Content-type': 'application/json' } };
-    return axios.put('/api/v1/password/update', passwords, config);
+    return axios.put(`${BASE_URI_VERSION}/password/update`, passwords, baseHeaders);
 }
 
 export function forgotPassword(email) {
-    const config = { headers: { 'Content-type': 'application/json' } };
-    return axios.post('/api/v1/password/forgot', email, config);
+    return axios.post(`${BASE_URI_VERSION}/password/forgot`, email, baseHeaders);
 }
 
 export function resetPassword(token, passwords) {
-    const config = { headers: { 'Content-type': 'application/json' } };
-    return axios.put(`/api/v1/password/reset/${token}`, passwords, config);
+    return axios.put(`${BASE_URI_VERSION}/password/reset/${token}`, passwords, baseHeaders);
 }
 
 export function getAllUsers() {
-    return axios.get('/api/v1/admin/users');
+    return axios.get(`${BASE_URI_VERSION}/admin/users`);
 }
 
 export function getUserDetails(id) {
-    return axios.get(`/api/v1/admin/user/${id}`);
+    return axios.get(`${BASE_URI_VERSION}/admin/user/${id}`);
 }
 
 export function updateUser(id, userData) {
-    const config = { headers: { 'Content-type': 'application/json' } };
-    return axios.put(`/api/v1/admin/user/${id}`, userData, config);
+    return axios.put(`${BASE_URI_VERSION}/admin/user/${id}`, userData, baseHeaders);
 }
 
 export function deleteUser(id) {
-    return axios.delete(`/api/v1/admin/user/${id}`);
+    return axios.delete(`${BASE_URI_VERSION}/admin/user/${id}`);
 }
 
 export function addItemToCart(id) {
-    return axios.get(`/api/v1/product/${id}`);
+    return axios.get(`${BASE_URI_VERSION}/product/${id}`);
 }
 
 export function createOrder(order) {
-    const config = { headers: { 'Content-Type': 'application/json' } };
-    return axios.post('/api/v1/order/new', order, config);
+    return axios.post(`${BASE_URI_VERSION}/order/new`, order, baseHeaders);
 }
 
 export function myOrders() {
-    return axios.get('/api/v1/orders/me');
+    return axios.get(`${BASE_URI_VERSION}/orders/me`);
 }
 
 export function getOrderDetails(id) {
-    return axios.get(`/api/v1/order/${id}`);
+    return axios.get(`${BASE_URI_VERSION}/order/${id}`);
 }
 
 export function getAllOrders() {
-    return axios.get(`/api/v1/admin/orders`);
+    return axios.get(`${BASE_URI_VERSION}/admin/orders`);
 }
 
 export function updateOrder(id, orderData) {
-    const config = { headers: { 'Content-Type': 'application/json' } };
-    return axios.put(`/api/v1/admin/order/${id}`, orderData, config);
+    return axios.put(`${BASE_URI_VERSION}/admin/order/${id}`, orderData, baseHeaders);
 }
 
 export function deleteOrder(id) {
-    return axios.delete(`/api/v1/admin/order/${id}`);
+    return axios.delete(`${BASE_URI_VERSION}/admin/order/${id}`);
 }
 
 export function getProducts(keyword, currentPage, minPrice, maxPrice) {
-    const link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[lte]=${maxPrice}&price[gte]=${minPrice}`;
+    const link = `${BASE_URI_VERSION}/products?keyword=${keyword}&page=${currentPage}&price[lte]=${maxPrice}&price[gte]=${minPrice}`;
     return axios.get(link);
 }
 
 export function getProductDetails(id) {
-    return axios.get(`/api/v1/product/${id}`);
+    return axios.get(`${BASE_URI_VERSION}/product/${id}`);
 }
 
 export function newReview(reviewData) {
-    const config = { headers: { 'Content-Type': 'application/json' } };
-    return axios.put('/api/v1/review', reviewData, config);
+    return axios.put(`${BASE_URI_VERSION}/review`, reviewData, baseHeaders);
 }
 
 export function getAdminProducts() {
-    return axios.get('/api/v1/admin/products');
+    return axios.get(`${BASE_URI_VERSION}/admin/products`);
 }
 
 export function newProduct(productData) {
-    const config = { headers: { 'Content-Type': 'application/json' } };
-    return axios.post('/api/v1/admin/product/new', productData, config);
+    return axios.post(`${BASE_URI_VERSION}/admin/product/new`, productData, baseHeaders);
 }
 
 export function deleteProduct(id) {
-    return axios.delete(`/api/v1/admin/product/${id}`);
+    return axios.delete(`${BASE_URI_VERSION}/admin/product/${id}`);
 }
 
 export function updateProduct(id, productData) {
-    const config = { headers: { 'Content-Type': 'application/json' } };
-    return axios.put(`/api/v1/admin/product/${id}`, productData, config);
+    return axios.put(`${BASE_URI_VERSION}/admin/product/${id}`, productData, baseHeaders);
 }
 
 export function getProductReviews(id) {
-    return axios.get(`/api/v1/reviews?id=${id}`);
+    return axios.get(`${BASE_URI_VERSION}/reviews?id=${id}`);
 }
 
 export function deleteReview(id, productId) {
-    return axios.delete(`/api/v1/reviews?id=${id}&productId=${productId}`);
+    return axios.delete(`${BASE_URI_VERSION}/reviews?id=${id}&productId=${productId}`);
 }
 
 export function getStripeApi() {
-    return axios.get('api/v1/stripeapi');
+    return axios.get(`${BASE_URI_VERSION}/stripeapi`);
 }
 
 export function processPayment(paymentData) {
-    const config = { headers: { 'Content-Type': 'application/json' } };
-    return axios.post('/api/v1/payment/process', paymentData, config);
+    return axios.post(`${BASE_URI_VERSION}/payment/process`, paymentData, baseHeaders);
 }
